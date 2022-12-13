e8ad5a9c95d342b1ad5e4ca41159a3230ef11d2d
Mujtaba <118379320+frozenPigeons@users.noreply.github.com>
2022-12-12 22:16:59 -0500

    Add files via upload

--- colourGenerator.py
+++ colourGenerator.py
@@ -2,11 +2,20 @@ import random
 import cv2
 import numpy as np
 
-type_of_generation = random.randint(1, 6)
+type_of_generation = random.randint(1, 4)
+#type_of_generation = 4
+print(type_of_generation)
 
 #dimensions of the canvas
 height = 500
 width = 500
+
+if type_of_generation == 4:
+    height = 600
+    width = 600
+
+white = [255, 255, 255]
+black = [0, 0, 0]
 
 #generates a random rgb colour
 def rgb():
@@ -25,7 +34,6 @@ def avgColour(colour_1, colour_2):
     return newColour
 
 img = np.zeros((height,width,3), np.uint8)
-type_of_generation = 2
 
 
 #complete random!!
@@ -59,6 +67,51 @@ if type_of_generation == 2:
                     img[j][k] = colour_2
     cv2.imshow('Image', img)
 
+#monochromatic
+#consider chaning the lightest and darkest colours to more midtones
+if type_of_generation == 3:
+    colour_1 = rgb()
+    midLight = avgColour(white, colour_1)
+    midDark = avgColour(colour_1, black)
+    lightest = avgColour(midLight, white)
+    darkest = avgColour(midDark, black)
+    for i in range(5):
+        for j in range(500-i*100):
+            for k in range(img.shape[1]):
+                if i == 0:
+                    img[j][k] = lightest
+                if i == 1:
+                    img[j][k] = midLight
+                if i == 2:
+                    img[j][k] = colour_1
+                if i == 3:
+                    img[j][k] = midDark
+                if i == 4:
+                    img[j][k] = darkest
+    cv2.imshow('Image', img)
 
+#monochromatic with three mid tones
+if type_of_generation == 4:
+    colour_1 = rgb()
+    midLight = avgColour(white, colour_1)
+    midDark = avgColour(colour_1, black)
+    lightest = avgColour(midLight, white)
+    darkest = avgColour(midDark, black)
+    for i in range(6):
+        for j in range(600-i*100):
+            for k in range(img.shape[1]):
+                if i == 0:
+                    img[j][k] = black
+                if i == 1:
+                    img[j][k] = avgColour(white, black)
+                if i == 2:
+                    img[j][k] = white
+                if i == 3:
+                    img[j][k] = midDark
+                if i == 4:
+                    img[j][k] = colour_1
+                if i == 5:
+                    img[j][k] = midLight
+    cv2.imshow('Image', img)
 
 cv2.waitKey(0)
